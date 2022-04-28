2a16812ec51ee557bb539fddc8dbf0c4c84db685
damt125 <damt125@36-09.aglinformatica.es>
2022-04-28 18:56:33 +0200

    (Version 1.0)

---
+++ src/rodriguez1ejemplogithub/Rodriguez1EjemploGitHub.java
@@ -0,0 +1,10 @@
+
+package rodriguez1ejemplogithub;
+
+public class Rodriguez1EjemploGitHub {
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println("Adrián no sabe nada de ficheros — V2.0");
+        }
+    }
+}
